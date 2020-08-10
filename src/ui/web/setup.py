95b04e664a6e41f92e0dd085aa3a1a38d0c0e041
Pablo Daniel Rey <pablodanielrey@gmail.com>
2020-08-10 18:10:36 -0300

    mas reestructuración

---
+++ src/ui/web/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages, find_namespace_packages
+
+setup(name="vete_models",
+    version="0.0.1",
+    package_dir={"":"src"},
+    packages=find_namespace_packages(where="src"),
+    install_requires=[
+        'vete_entities',
+        'inject',
+        'SQLAlchemy',
+        'Flask',
+        'flask-wtf',
+        'psycopg2-binary'
+    ]
+)
